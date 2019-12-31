f40b5fe60930c42fe146c3ccc79d95592d4db85c
Terran Blake <terranblake@gmail.com>
2019-12-31 07:17:22 -0800

    add filings and companies endpoints

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -6,6 +6,27 @@ const on = require('await-on');
 const { metadata } = require('@postilion/utils');
 const { Company } = require('@postilion/models');
 
+router.get('/:id', async (req, res) => {
+    const { params = {} } = req;
+    const { company = '' } = params;
+
+    const [companyError, companyObject] = await on(Company.findById(company));
+    if (companyError) {
+        return res.status(400).send({
+            error: `unable to find company with id ${company}`
+        });
+    }
+
+    if (companyObject) {
+        return res.status(200).send(companyObject);
+    }
+
+    return res.status(400).send({
+        message: `unable to find company with that id`
+    });
+});
+
+// todo: add query parser to base routes for more complex queries
 router.get('/', async (req, res) => {
     const { query = {} } = req;
     const { ticker } = query;
---
+++ src/routes/filings.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const router = express.Router({ mergeParams: true });
+
+const on = require('await-on');
+const { Filing, Company } = require('@postilion/models');
+
+router.get('/:id', async (req, res) => {
+    const { params = {} } = req;
+    const { filing = '' } = params;
+
+    const [filingError, filingObject] = await on(Filing.findById(filing));
+    if (filingError) {
+        return res.status(400).send({
+            error: `unable to find filing with id ${company}`
+        });
+    }
+
+    if (filingObject) {
+        return res.status(200).send(filingObject);
+    }
+
+    return res.status(400).send({
+        message: `unable to find filing with that id`
+    });
+});
+
+router.get('/', async (req, res) => {
+    const { query = {} } = req;
+    const { company = '' } = query;
+
+    const [companyError] = await on(Company.findById(company));
+    if (companyError) {
+        return res.status(400).send({
+            error: `unable find company with id ${company}`
+        });
+    }
+
+    const filings = await Filing.find({ company }).lean();
+    if (filings) {
+        return res.status(200).send(filings);
+    }
+
+    return res.status(400).send({
+        message: 'unable to find filings for that company'
+    })
+});
+
+module.exports = router;
